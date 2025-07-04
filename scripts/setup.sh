a27fe5e7e0f62e3901121484f83ce4a0d98f9d90
ciroautuori <ciroautuoribcn@gmail.com>
2025-07-04 19:14:18 +0200

    fix: Add flatpak to pacman install list

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -31,7 +31,7 @@ log "Script eseguito per l'utente: $TARGET_USER"
 
 # --- Installazione Pacchetti di Sistema (pacman) ---
 log "Installazione dei pacchetti di sistema necessari..."
-pacman -S --noconfirm --needed git base-devel cpupower nvidia nvidia-settings python-pipx python-setuptools
+pacman -S --noconfirm --needed git base-devel cpupower nvidia nvidia-settings python-pipx python-setuptools flatpak
 
 # --- Installazione di 'yay' (AUR Helper) ---
 log "Installazione di 'yay' per la gestione dell'AUR..."
